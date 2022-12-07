63024b09c4622421723834de4a04932eb24454d6
김현교 <zkdlajt96@naver.com>
2022-12-07 12:25:09 +0900

    learning test

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,44 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class StringTest {
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    void split1() {
+        String[] actual = "1,2".split(",");
+
+        assertThat(actual).contains("1");
+        assertThat(actual).contains("2");
+        assertThat(actual).containsExactly("1", "2");
+    }
+
+    @Test
+    void subString() {
+        String substring = "(1,2)".substring(1, 4);
+        assertThat(substring).isEqualTo("1,2");
+    }
+
+    @Test
+    void charAt() {
+        assertThat("abc".charAt(0)).isEqualTo('a');
+        assertThat("abc".charAt(1)).isEqualTo('b');
+        assertThat("abc".charAt(2)).isEqualTo('c');
+    }
+
+    @Test
+    @DisplayName("Exception 테스트")
+    void charAtException() {
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(() -> "abc".charAt(3));
     }
 }
